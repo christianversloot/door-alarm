9222b122defd93d8386f0a7c164034408f04b537
Christian Versloot <mail@christianversloot.nl>
2021-05-18 19:34:29 +0200

    Cleanup

--- frontend/src/app/accord/accord.service.ts
+++ frontend/src/app/accord/accord.service.ts
@@ -31,15 +31,9 @@ export class AccordService {
       console.log(sub)
       const pkey = sub.getKey('p256dh');
       const akey = sub.getKey('auth');
-      //{"endpoint":"https://fcm.googleapis.com/fcm/send/d7Y_91EjI1M:APA91bGcRoJQjoWbxL_VmYDN45m6bBKD_-NOhXBQ700g9qzmmC1366fpylfYY0B4HLYG2uz7MYeeWwm5AtXHw1fxxGsOG3r-fvk-Ftgqq5LJvkKjaBzay9037dVwvFAZOHuoJw7JGS04","expirationTime":null,"keys":{"p256dh":"","auth":""}}
-      console.log('keys',pkey,akey);
       return this.showSnackbar('Push notifications are now enabled.');
     })
     .catch(err => this.showSnackbar(err));
-  }
-
-  ab2str(buf) {
-    return String.fromCharCode.apply(null, new Uint16Array(buf));
   }
 
   /** Show snackbar */
