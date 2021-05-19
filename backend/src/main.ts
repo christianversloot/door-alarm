ad2874f7645a53ea1c52bc160d13db39edb0b368
Christian Versloot <mail@christianversloot.nl>
2021-05-19 17:48:22 +0200

    Add .env to Compose file

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,6 @@ import { AppModule } from './app.module';
 import config from './config';
 
 async function bootstrap() {
-  require('dotenv').config()
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(config.port);
