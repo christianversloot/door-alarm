fc63f3d08b67d3856b679fc293bd801c0f63cd55
Christian Versloot <mail@christianversloot.nl>
2021-05-19 19:33:29 +0200

    Remove environment prod file

--- frontend/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  vapid_public_key:"BI_ybwmu3Ql531LggQX3ojyWtOEKWysgZtvRuNvnpqT2B3litPv70xFUfl-ANWAPU1U32OU16hKP3EU_jckxAIQ",
-  backend_endpoint: "http://localhost:3000"
-};
